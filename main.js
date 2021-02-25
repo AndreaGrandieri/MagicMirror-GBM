84a3429f5807c331d1d4999ebdfcc4cecc169a19
Andrea Grandieri <g.andreus02@gmail.com>
2021-02-25 21:20:10 +0100

    Download latest release should be ok

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 function getLatestRelease(apiURL) {
-    $.getJSON(apiURL, function (json) {
-        console.log(apiURL);
-        console.log(json);
-
-        //  var obj = JSON.parse(json);
-        //   var zipball_url = obj.zipball_url;
-
-        var zipball_url = json.zipball_url;
-
-        //   console.log(obj);
-
+    $.getJSON(apiURL, function (obj) {
+        var zipball_url = obj.zipball_url;
         if (zipball_url != null && zipball_url != undefined) {
             window.open(obj.zipball_url);
         }
