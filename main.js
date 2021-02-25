dda50a7753aef7e91894f314770b0ccd3b259be2
Andrea Grandieri <g.andreus02@gmail.com>
2021-02-25 21:12:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 function getLatestRelease(apiURL) {
     $.getJSON(apiURL, function (json) {
+        console.log(apiURL);
+        console.log(json);
+
         var obj = JSON.parse(json);
         var zipball_url = obj.zipball_url;
+
+        console.log(obj);
 
         if (zipball_url != null && zipball_url != undefined) {
             window.open(obj.zipball_url);
