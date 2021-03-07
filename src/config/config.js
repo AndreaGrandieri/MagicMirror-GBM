de6ef708006cd63385e4d83a9d38c6149501e7c9
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-07 16:42:27 +0100

    test icone

--- src/config/config.js
+++ src/config/config.js
@@ -120,7 +120,7 @@ var config = {
 			config: {
 				displaySymbol: true,
 				symbolOnline: "wifi",
-				symbolOffline: "address-book",
+				symbolOffline: "wifi",
 				colored: true,
 				colorOnline: "#FFFFFF",
 				colorOffline: "#FF0000"
