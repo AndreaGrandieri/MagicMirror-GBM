fc553efe5833efdcc9f395527c8dc0633e23d45b
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-07 16:50:51 +0100

    test icone e analisi ambiguità codice sorgente
    Ambiguità nell'interpretazione delle icone

--- src/config/config.js
+++ src/config/config.js
@@ -119,8 +119,8 @@ var config = {
 			position: 'top_right',
 			config: {
 				displaySymbol: true,
-				symbolOnline: "fas fa-globe",
-				symbolOffline: "fas fa-globe",
+				symbolOnline: "globe",
+				symbolOffline: "globe",
 				colored: true,
 				colorOnline: "#FFFFFF",
 				colorOffline: "#FF0000"
