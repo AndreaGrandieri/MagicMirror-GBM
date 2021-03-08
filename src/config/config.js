7e506e9cdd9b45330f97636333f4751ca83be0eb
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-08 22:21:19 +0100

    +forecast

--- src/config/config.js
+++ src/config/config.js
@@ -86,6 +86,29 @@ var config = {
 			}
 		},
 		{
+			module: "weather",
+			position: "top_right",
+			config: {
+				weatherProvider: "openweathermap",
+				type: "forecast",
+				units: "metric",
+				degreeLabel: true,
+				updateInterval: 600000,
+				lang: "en",
+				initialLoadDelay: 1000,
+				onlyTemp: false,
+				showHumidity: true,
+				showIndoorTemperature: true,
+				showIndoorHumidity: true,
+				showSun: true,
+				colored: true,
+				showPrecipitationAmount: true,
+				maxNumberOfDays: 5,
+				locationID: "3173435",
+				apiKey: "f41537e389176cf93d8e0586d0d701ae"
+			}
+		},
+		{
 			module: "newsfeed",
 			position: "bottom_bar",
 			config: {
