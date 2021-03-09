26114fa6c52290481d55404f3301e2973935aa66
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-09 10:12:34 +0100

    Documento per weatherforecast steso (prima stesura). Inizio testing (compilato config.js)

--- src/config/config.js
+++ src/config/config.js
@@ -97,14 +97,22 @@ var config = {
 				apiKey: "f41537e389176cf93d8e0586d0d701ae"
 			}
 		},
-		/*{
+		{
 			module: "weatherforecast",
 			position: "top_right",
 			config: {
+				units: "metric",
+				updateInterval: 600000,
+				lang: "en",
+				initialLoadDelay: 1000,
+				colored: true,
+				showRainAmount: true,
+				maxNumberOfDays: 5,
+				roundTemp: true,
 				locationID: "3173435",
 				appid: "f41537e389176cf93d8e0586d0d701ae"
 			}
-		},*/
+		},
 		{
 			module: "newsfeed",
 			position: "bottom_bar",
