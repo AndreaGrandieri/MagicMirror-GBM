96a7dc64bd968cc89d9431a7d758b13253145a96
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-09 22:28:53 +0100

    Definizione "main" && "moduliSelector". Grande cambio!!!!
    Grande cambio: #119

---
+++ src/ipdashboard/php/main.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<style type="text/css">
+    @import url(../css/style.css);
+</style>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MagicMirror-GBM IP Dashboard</title>
+</head>
+
+<body>
+
+    <h1>MagicMirror-GBM IP Dashboard</h1>
+
+    <!-- Tabella delle scelte -->
+    <table style="width:60%">
+        <tr>
+            <th>Opzione</th>
+            <th>Descrizione</th>
+        </tr>
+        <tr>
+            <td><a href="moduliSelector.php">Moduli</a></td>
+            <td>Modifica i moduli in attività per il MagicMirror.</td>
+        </tr>
+    </table>
+</body>
+
+</html>
---
+++ src/ipdashboard/php/moduliSelector.php
@@ -0,0 +1,60 @@
+<?php
+// Compilazione dinamica tabella dei moduli
+// sulla base di "config.js"
+
+$filePath = "../../config/config.json";
+
+$file = fopen($filePath, "r") or die("Unable to parse 'config.json'");
+$jsonContent = fread($file, filesize($filePath));
+$jsonParsed = json_decode($jsonContent, true);
+$jsonParsedModuli = $jsonParsed["config"]["modules"];
+
+$dynTable = "
+<table style='width:60%'>
+<tr>
+    <th>Nome Modulo</th>
+    <th>Attiva / Disattiva</th>
+</tr>
+";
+
+for ($i = 0; $i < count($jsonParsedModuli); $i++) {
+    $nomeModulo = $jsonParsedModuli[$i]["module"];
+    $dynTable .= "
+            <tr>
+            <td>$nomeModulo</td>
+            <td>'CHECKBOX'</td>
+        </tr>
+    ";
+}
+
+$dynTable .= "</table>"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<style type="text/css">
+    @import url(../css/style.css);
+</style>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moduli Selector</title>
+</head>
+
+<body>
+
+    <h1>Moduli Selector</h1>
+
+    <div>
+        <?php
+        echo $dynTable;
+        ?>
+    </div>
+
+</body>
+
+</html>
