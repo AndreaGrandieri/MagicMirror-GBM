2dc711466fde762c317b3d9477b7e154ea33080f
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-10 14:32:49 +0100

    Definito "editorstable1.php"

--- src/ipdashboard/php/editorstable1.php
+++ src/ipdashboard/php/editorstable1.php
@@ -9,34 +9,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Editor</title>
+    <title>Editor Stable 1</title>
 </head>
 
 <body>
-
-
     <!-- Loading codemirror -->
     <script src="../codemirror-5.59.4/lib/codemirror.js"></script>
     <link rel="stylesheet" href="../codemirror-5.59.4/lib/codemirror.css">
     <script src="../codemirror-5.59.4/mode/javascript/javascript.js"></script>
-
-    <!-- Loading JSON linter -->
-    <!--
-    <link rel="stylesheet" href="../codemirror-5.59.4/addon/lint/lint.css">
-    <script src="../codemirror-5.59.4/addon/lint/lint.js"></script>
-    <script src="../codemirror-5.59.4/addon/lint/json-lint.js"></script>
--->
-
-    <!-- Loading require -->
-    <script src="../js/require.js"></script>
-
-    <!-- Loading JSON linter dependencies -->
-    <script>
-        window.jsonlint = require("../jsonlint/lib/jsonlint.js");
-        require("../codemirror-5.59.4/addon/lint/lint.css");
-        require("../codemirror-5.59.4/addon/lint/lint.js");
-        require("../codemirror-5.59.4/addon/lint/json-lint.js");
-    </script>
 
     <textarea id="editorTextArea">
         </textarea>
@@ -49,8 +29,6 @@
             value: "{\n}",
             mode: "application/json",
             lineNumbers: true,
-            gutters: ['CodeMirror-lint-markers'],
-            lint: true
         });
         myCodeMirror.setSize(700, 300);
     </script>
