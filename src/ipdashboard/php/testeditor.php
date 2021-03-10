482f7ed6c7600ea1742a199b2d514f0a841e8a4b
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-10 11:38:29 +0100

    Ripristino stato stabile

--- src/ipdashboard/php/testeditor.php
+++ src/ipdashboard/php/testeditor.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<style type="text/css">
+    @import url(../css/style.css);
+</style>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
