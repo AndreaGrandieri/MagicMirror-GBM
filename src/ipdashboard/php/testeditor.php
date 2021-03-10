268f2efb55065bad6f06a0e7da32e81bff70fa0b
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-10 08:51:26 +0100

    Aggiunto codice src per codemirror https://codemirror.net/index.html

---
+++ src/ipdashboard/php/testeditor.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Editor</title>
+</head>
+
+<body>
+
+    <script src="../codemirror-5.59.4/lib/codemirror.js"></script>
+    <link rel="stylesheet" href="../codemirror-5.59.4/lib/codemirror.css">
+    <script src="../codemirror-5.59.4/mode/javascript/javascript.js"></script>
+
+    <textarea id="textarea">
+        </textarea>
+
+    <script>
+        var myCodeMirror = CodeMirror(function(elt) {
+            var textarea = document.getElementById("textarea");
+            textarea.parentNode.replaceChild(elt, textarea);
+        }, {
+            value: "{\n}",
+            mode: "application/json",
+            lineNumbers: true
+        });
+        myCodeMirror.setSize(500, 300);
+    </script>
+
+</body>
+
+</html>
