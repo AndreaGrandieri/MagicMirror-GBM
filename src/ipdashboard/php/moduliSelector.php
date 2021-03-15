ec03e752daa5947c8afd8a5188530041c839c4f5
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-15 10:49:35 +0100

    RenderIndex implementati. Il funzionamento sembra corretto. Ulteriori test a venire...
    GOOD
    
    @AndreaGrandieri

--- src/ipdashboard/php/moduliSelector.php
+++ src/ipdashboard/php/moduliSelector.php
@@ -22,24 +22,45 @@ $dynTable = "
 <tr>
     <th>Nome Modulo</th>
     <th>Attiva / Disattiva</th>
+    <th>Indice</th>
 </tr>
 ";
 
 // Interrogo tabella "modules"
-$results = $db->query("SELECT NomeModulo, Active FROM modules ORDER BY RenderIndex");
+$results = $db->query("SELECT NomeModulo, Active, RenderIndex FROM modules ORDER BY RenderIndex");
+$results2 = $db->query("SELECT COUNT (*) AS NumRows FROM modules");
+if (gettype($results2) !== "boolean") {
+    $numRows = $results2->fetchArray(SQLITE3_ASSOC)["NumRows"];
+} else {
+    setSessionVariable("statusPHP", "Error while querying the database.");
+    setSessionVariable("statusPHPRedirect", null);
+    header("location: redirect.php?target=index.php&ms=300");
+    die;
+}
+
+$indexes = array();
 if (gettype($results) !== "boolean") {
     while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
         $nomeModulo = $row["NomeModulo"];
         $dynTable .= "
             <tr>
-            <td><a href='moduloSettings.php?nomeModulo=$nomeModulo'>$nomeModulo</a></td>
+            <td>
+            <a href='moduloSettings.php?nomeModulo=$nomeModulo'>$nomeModulo</a>
+            <input type='checkbox' id='$nomeModulo' name='refOrdineModuli[]' value='$nomeModulo' checked hidden>
+            </td>
     ";
 
         if ($row["Active"]) {
-            $dynTable .= "<td><input type='checkbox' id='$nomeModulo' name='attivazione[]' value='$nomeModulo' checked></td></tr>";
+            $dynTable .= "<td><input type='checkbox' id='$nomeModulo' name='attivazione[]' value='$nomeModulo' checked></td>";
         } else {
-            $dynTable .= "<td><input type='checkbox' id='$nomeModulo' name='attivazione[]' value='$nomeModulo'></td></tr>";
+            $dynTable .= "<td><input type='checkbox' id='$nomeModulo' name='attivazione[]' value='$nomeModulo'></td>";
         }
+
+        $id = $nomeModulo . "RenderIndex";
+        $index = $row["RenderIndex"];
+        // array_push($indexes, $index);
+        $max = $numRows - 1;
+        $dynTable .= "<td><input type='number' id='$nomeModulo' name='indici[]' value='$index' min='0' max='$max' required></td></tr>";
     }
 } else {
     setSessionVariable("statusPHP", "Error while querying the database.");
@@ -48,6 +69,7 @@ if (gettype($results) !== "boolean") {
     die;
 }
 
+// asort($indexes);
 $dynTable .= "</table>";
 
 // Ottengo "statusPHP"
