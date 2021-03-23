b7cbe843f5ae8d99f05f35974051f6a842e6f3eb
Andrea Grandieri <g.andreus02@gmail.com>
2021-03-23 23:10:02 +0100

    Aggiunti controlli per bloccare tentativo di aggiornamento con branch senza riferimento remoto

--- src/ipdashboard/php/aggiornamentoSoftware.php
+++ src/ipdashboard/php/aggiornamentoSoftware.php
@@ -13,24 +13,37 @@ exec("git symbolic-ref HEAD --short", $output);
 
 $branch = $output[0];
 
-// Controllo la presenza di aggiornamenti software
-// (aka) Controlla il numero di commits di cui il "checked_out_branch"
-// è behind in confronto a "origin/checked_out_branch"
+// Controllo l'esistenza del branch in remoto, altrimenti
+// non ha senso parlare di aggiornamento
 $output = array();
-exec("git fetch");
-exec("git rev-list --left-only --count origin/$branch...$branch", $output);
+exec("git ls-remote --heads origin $branch", $output);
 
-if ($output[0] == 0) {
-    // Nessun aggiornamento disponibile
-    $button = '<input type="submit" id="submit" name="submit" value="AGGIORNA" disabled>';
+if (!array_key_exists(0, $output)) {
+    // Il branch checked out è SOLO LOCALE
+    // Discussione di aggiornamenti non possibile in questo contesto
+    $isScopeValid = false;
 } else {
-    // Aggiornamento disponibile
-    $button = '<input type="submit" id="submit" name="submit" value="AGGIORNA">';
-}
-if ($output[0] == 1) {
-    $commits = "$output[0] commit";
-} else {
-    $commits = "$output[0] commits";
+    $isScopeValid = true;
+
+    // Controllo la presenza di aggiornamenti software
+    // (aka) Controlla il numero di commits di cui il "checked_out_branch"
+    // è behind in confronto a "origin/checked_out_branch"
+    $output = array();
+    exec("git fetch");
+    exec("git rev-list --left-only --count origin/$branch...$branch", $output);
+
+    if ($output[0] == 0) {
+        // Nessun aggiornamento disponibile
+        $button = '<input type="submit" id="submit" name="submit" value="AGGIORNA" disabled>';
+    } else {
+        // Aggiornamento disponibile
+        $button = '<input type="submit" id="submit" name="submit" value="AGGIORNA">';
+    }
+    if ($output[0] == 1) {
+        $commits = "$output[0] commit";
+    } else {
+        $commits = "$output[0] commits";
+    }
 }
 
 setSessionVariable("statusPHP", null);
@@ -54,17 +67,20 @@ setSessionVariable("statusPHPRedirect", null);
     del software del MagicMirror aggiornato all'ultima versione, se la versione
     correntemente installata risulta datata.
 
-    <br><br>
+    <br>
 
+    <?php
+    if ($isScopeValid) {
+        echo '<br>
     <div id="commitsCounter">
         <span style="color: blue"><b>remote <?php echo "(origin/$branch)" ?></b></span> è avanti di
-        <?php echo "<span style='color: green'><b>$commits</b></span>" ?>
+        <?php echo "<span style=\"color: green\"><b>$commits</b></span>" ?>
         in confronto a
         <span style="color: darkviolet"><b>local <?php echo "($branch)" ?></b></span>.
     </div>
 
     <p style="color: red">
-        <b>Durante l'aggiornamento software, NON chiudere la finestra del browser! Al termine
+        <b>Durante l\'aggiornamento software, NON chiudere la finestra del browser! Al termine
             della procedura si verrà riportati alla schermata principale automaticamente.</b>
     </p>
 
@@ -74,7 +90,14 @@ setSessionVariable("statusPHPRedirect", null);
             echo $button
             ?>
         </div>
-    </form>
+    </form>';
+    } else {
+        echo '<p><span style="color: darkviolet"><b>local <?php echo "($branch)" ?></b></span> non ha
+        un riferimento remoto come fonte degli aggiornamenti. Dunque, non è possibile procedere.
+        Effettuare il checkout di un altro <span style="color: darkviolet"><b>local</b></span> che abbia
+        un riferimento remoto.</p>';
+    }
+    ?>
 
     <br>
     <a href="index.php">Home Page</a><br><br>
