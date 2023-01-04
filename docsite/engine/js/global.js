c1ffa7c910d13757512d16b8fe51583ab43ecf3e
Andrea Grandieri <g.andreus02@gmail.com>
2023-01-04 18:04:27 +0100

    RepoLink: docsite assets updated

--- docsite/engine/js/global.js
+++ docsite/engine/js/global.js
@@ -8,13 +8,13 @@
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 
-// This is a module. The globalThis export is used.
+// This is a module. The globalThis export is used. The globalThis export can also be used with variables.
 
 // Global variables
-var light_name = "custom_light"
-var dark_name = "custom_dark"
-var thirdOrderBrowserArray = []
-globalThis.thirdOrderBrowserArray = thirdOrderBrowserArray
+var light_name = "custom_light";
+var dark_name = "custom_dark";
+var thirdOrderBrowserArray = [];
+globalThis.thirdOrderBrowserArray = thirdOrderBrowserArray;
 
 // Function to switch between light and dark mode
 // The function should be called within a button
@@ -29,89 +29,100 @@ function themeModeSwitcher() {
   if (typeof (Storage) !== "undefined") {
     if (sessionStorage.theme) {
       if (sessionStorage.theme == light_name) {
-        toggleDarkMode()
+        toggleDarkMode();
       } else {
-        toggleLightMode()
+        toggleLightMode();
       }
     } else {
-      sessionStorage.theme = light_name
-      toggleDarkMode()
+      sessionStorage.theme = light_name;
+      toggleDarkMode();
     }
   } else {
     // Continuity won't work
     if (themeModeSwitcher.theme !== "undefined") {
       if (themeModeSwitcher.theme == light_name) {
-        toggleDarkMode()
+        toggleDarkMode();
       } else {
-        toggleLightMode()
+        toggleLightMode();
       }
     } else {
-      themeModeSwitcher.theme = light_name
-      toggleDarkMode()
+      themeModeSwitcher.theme = light_name;
+      toggleDarkMode();
     }
   }
 }
-globalThis.themeModeSwitcher = themeModeSwitcher
+globalThis.themeModeSwitcher = themeModeSwitcher;
 
 function toggleDarkMode() {
-  document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-sun fa-3x\"></i>"
+  document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-sun fa-3x\"></i>";
 
   // Checks if the "sessionStorage" object is supported by the browser
   if (typeof (Storage) !== "undefined") {
-    sessionStorage.theme = dark_name
-    jtd.setTheme(sessionStorage.theme)
+    sessionStorage.theme = dark_name;
+    jtd.setTheme(sessionStorage.theme);
   } else {
     // Continuity won't work
-    themeModeSwitcher.theme = dark_name
-    jtd.setTheme(themeModeSwitcher.theme)
+    themeModeSwitcher.theme = dark_name;
+    jtd.setTheme(themeModeSwitcher.theme);
   }
 }
-globalThis.toggleDarkMode = toggleDarkMode
+globalThis.toggleDarkMode = toggleDarkMode;
 
 function toggleLightMode() {
-  document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-moon fa-3x\"></i>"
+  document.getElementById("lightdarkSwitcherButton").innerHTML = "<i class=\"fa-solid fa-moon fa-3x\"></i>";
 
   // Checks if the "sessionStorage" object is supported by the browser
   if (typeof (Storage) !== "undefined") {
-    sessionStorage.theme = light_name
-    jtd.setTheme(sessionStorage.theme)
+    sessionStorage.theme = light_name;
+    jtd.setTheme(sessionStorage.theme);
   } else {
     // Continuity won't work
-    themeModeSwitcher.theme = light_name
-    jtd.setTheme(themeModeSwitcher.theme)
+    themeModeSwitcher.theme = light_name;
+    jtd.setTheme(themeModeSwitcher.theme);
   }
 }
-globalThis.toggleLightMode = toggleLightMode
+globalThis.toggleLightMode = toggleLightMode;
 
 function retrieveTheme() {
   // Checks if the "sessionStorage" object is supported by the browser
   if (typeof (Storage) !== "undefined") {
     if (sessionStorage.theme) {
       if (sessionStorage.theme == light_name) {
-        toggleLightMode()
+        toggleLightMode();
       } else {
-        toggleDarkMode()
+        toggleDarkMode();
       }
     }
   } else {
     // Continuity won't work
     if (themeModeSwitcher.theme !== "undefined") {
       if (themeModeSwitcher.theme == light_name) {
-        toggleLightMode()
+        toggleLightMode();
       } else {
-        toggleDarkMode()
+        toggleDarkMode();
       }
     }
   }
 }
-globalThis.retrieveTheme = retrieveTheme
+globalThis.retrieveTheme = retrieveTheme;
 
-function appendToThirdOrderBrowserArray(element) {
-  thirdOrderBrowserArray.push(element)
+function appendToThirdOrderBrowserArray(string) {
+  thirdOrderBrowserArray.push(string);
 }
-globalThis.appendToThirdOrderBrowserArray = appendToThirdOrderBrowserArray
+globalThis.appendToThirdOrderBrowserArray = appendToThirdOrderBrowserArray;
 
 function injectHMTLToThirdOrderBrowserDiv() {
+  toInject = "<li class=\"aux-nav-list-item\"><a href=\"" + thirdOrderBrowserArray[1] + "\"" + "class=\"site-button\"";
+  if (thirdOrderBrowserArray[2] == true) {
+    toInject += " target=\"_blank\" rel=\"noopener noreferrer\"";
+  }
+  toInject += ">" + thirdOrderBrowserArray[0] + "</a></li>";
 
+  document.getElementById("thirdOrderBrowserDiv").innerHTML += toInject;
 }
-globalThis.injectHMTLToThirdOrderBrowserDiv = injectHMTLToThirdOrderBrowserDiv
+globalThis.injectHMTLToThirdOrderBrowserDiv = injectHMTLToThirdOrderBrowserDiv;
+
+function flushThirdOrderBrowserArray() {
+  thirdOrderBrowserArray = [];
+}
+globalThis.flushThirdOrderBrowserArray = flushThirdOrderBrowserArray;
